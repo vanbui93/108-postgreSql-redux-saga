28c01057afb0bf73da4019a9f89286357aec1295
Van Bui <van.bt@trans-cosmos.com.vn>
2020-06-23 16:52:30 +0700

    Connect Nodejs vs PostgreSql -> ok

--- task_manager/routes/connectDb.js
+++ task_manager/routes/connectDb.js
@@ -1,7 +1,5 @@
-// start connect API
-const {  Pool } = require('pg')
+const { Pool } = require('pg')
 const connectionString = 'postgres://awnbivlt:upgHKpim0LlUlNDrwfvjrJL29CHkUVT4@satao.db.elephantsql.com:5432/awnbivlt'
-const pool = new Pool({ connectionString, max: 25, idleTimeoutMillis: 1000})
-// end connect API
+const pool = new Pool({ connectionString, max: 25, idleTimeoutMillis: 1000 })
 
 module.exports = pool
--- task_manager/routes/index.js
+++ task_manager/routes/index.js
@@ -1,24 +1,30 @@
 var express = require('express');
 var router = express.Router();
+
 var pool = require('./connectDb');
 
 //thêm đoạn dưới vào để xử lý lỗi CORS PROXY
-// router.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-router.get('/', function(req, res) {
-  pool.connect(function(error){
-    pool.query('select * from tasks', (err, response) => {
-      if(error) {  // nếu lỗi thì trả về error
-        return res.send(err.message);
+
+/* GET product page. */
+router.get('/tasks', function (req, res) {
+  pool.connect(function (error) {
+    pool.query('SELECT * FROM tasks', (err, response) => {
+      if (error) {  // nếu lỗi thì trả về error
+        return console.error('error running query', error);
       } else {   // Nếu thành công trả về response
-        // console.log(response.rows); //console chỉ xem được trên backend thôi
-        return res.send(response.rows);
+        res.send(response.rows);  //send dữ liệu phía api
       }
       // pool.end(); // đóng cổng kết nói csdl
     })
   })
 });
+
+
+
+module.exports = router;
